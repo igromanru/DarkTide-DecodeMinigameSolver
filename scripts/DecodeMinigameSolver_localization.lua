241b8693da59ef6f812121f32ad6190aa4109680
igromanru <igromanru@yahoo.de>
2025-06-03 16:05:02 +0200

    feat: Add Chinese translation (mostly by jcyl2023)

--- scripts/DecodeMinigameSolver_localization.lua
+++ scripts/DecodeMinigameSolver_localization.lua
@@ -14,21 +14,27 @@ return {
   },
   mod_description =
   {
-    en = "Passes the Decode Symbol Minigame automatically"
+    en = "Passes the Decode Symbol Minigame automatically",
+    ["zh-cn"] = "自动通过解码符号迷你游戏",
   },
   [SettingNames.EnableMod] = {
-    en = "Enable"
+    en = "Enable",
+    ["zh-cn"] = "启用",
   },
   [SettingNames.InteractCooldown] = {
-    en = "Interaction Cooldown"
+    en = "Interaction Cooldown",
+    ["zh-cn"] = "互动冷却",
   },
   [SettingNames.InteractCooldownTooltip] = {
-    en = "Wait time between interactions in milliseconds. Default: 100"
+    en = "Wait time between interactions in milliseconds. Default: 100",
+    ["zh-cn"] = "互动冷却时间，默认100毫秒",
   },
   [SettingNames.TargetPrecision] = {
-    en = "Target Precision"
+    en = "Target Precision",
+    ["zh-cn"] = "精确度",
   },
   [SettingNames.TargetPrecisionTooltip] = {
-    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 3"
+    en = "The bigger the value, the smaller the detection window when the cursor is positioned on the symbol. Default: 3",
+    ["zh-cn"] = "该值越大，当光标位于符号上时，检测窗口越小。默认值：3",
   },
 }
